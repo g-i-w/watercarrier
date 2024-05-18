1c4097a7419ca1cc54ea66795ba57ea9b00e2116
Gabriel Wilson <giw@wilsons.life>
2024-05-18 14:44:04 -0500

    hopefully fileToDiskCopyGz is working now...

--- fileToDiskCopyGz.sh
+++ fileToDiskCopyGz.sh
@@ -1,11 +1,13 @@
-echo "extracting and cloning $1 to $2 ..."
+echo "Extracting and cloning '$1' to '$2'..."
 gunzip -c $1 | dd of=$2 bs=4M status=progress
-echo "creating mountpoint directory /mnt$2 ..."
-mkdir -p /mnt$2
-echo "mounting $2 at /mnt$2 ..."
-mount $2 /mnt$2
-echo "copying $1 to /mnt$2$1 ..."
-rsync --info=progress2 $1 /mnt$2$1
-echo "unmounting /mnt$2 ..."
-umount /mnt$2
+echo "Refreshing partitions on '$2'..."
+partprobe $2
+echo "Creating mountpoint directory '/mnt$22'..."
+mkdir -p /mnt$22
+echo "Mounting '$22' at '/mnt$22'..."
+mount $22 /mnt$22
+echo "Copying '$1' to '/mnt$22/home/servant/'..."
+rsync --info=progress2 $1 /mnt$22/home/servant/
+echo "Unmounting '/mnt$22'..."
+umount /mnt$22
 echo "Done."
